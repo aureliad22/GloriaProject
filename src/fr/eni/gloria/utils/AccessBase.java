d942815b02ab667b6f41e875cb4901ae48cb1e4e
Aurélia Delauné <aurelia.delaune@gmail.com>
2017-08-01 10:18:28 +0200

    Mise en place de la structure en couches du projet

---
+++ src/fr/eni/gloria/utils/AccessBase.java
@@ -0,0 +1,15 @@
+/**
+ * @author adelaune2017
+ * @date 1 août 2017
+ * @version GloriaProject V1.0
+ */
+package fr.eni.gloria.utils;
+
+/**
+ * @author adelaune2017
+ * @date 1 août 2017
+ * @version GloriaProject V1.0
+ */
+public class AccessBase {
+
+}
